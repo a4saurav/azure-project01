5d11fec7a991c44fb3a7529526b38c6663a9978d
Avinash Saurav <a4saurav@gmail.com>
2024-05-20 02:07:06 +0530

    terraform file added

--- main.tf
+++ main.tf
@@ -1,23 +1,48 @@
+
+# We strongly recommend using the required_providers block to set the
+# Azure Provider source and version being used
 terraform {
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
-      version = "3.104.0"
+      source  = "hashicorp/azurerm"
+      version = "=3.104.0"
     }
-    
   }
-  
 }
 
+# Configure the Microsoft Azure Provider
 provider "azurerm" {
-  features {
-     resource_group {
-      prevent_deletion_if_contains_resources = true
-    }
-  }
+  features {}
 }
 
-resource "azurerm_resource_group" "myresourcegroup" {
-    name = "resourcegroup01"
-    location = "eastus"
+
+
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "West Europe"
+}
+
+resource "azurerm_container_group" "example1" {
+  name                = "example-continst"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  ip_address_type     = "Public"
+  dns_name_label      = "aci-label123"
+  os_type             = "Linux"
+
+  container {
+    name   = "hello-world"
+    image  = "mcr.microsoft.com/mcr/hello-world"
+    cpu    = "0.5"
+    memory = "1.5"
+
+    ports {
+      port     = 443
+      protocol = "TCP"
+    }
+  }
+
+  tags = {
+    environment = "testing"
+  }
 }
